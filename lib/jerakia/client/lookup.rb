6a6ae8177b91e7a486fb364de7a19a21d4b4c6f2
Craig Dunn <craig@craigdunn.org>
2017-01-14 11:39:11 +0100

    added handling of scope and metadata hashes

--- lib/jerakia/client/lookup.rb
+++ lib/jerakia/client/lookup.rb
@@ -2,6 +2,19 @@ class Jerakia
   class Client
     module Lookup
       def lookup(key, params)
+        if params[:scope_opts].is_a?(Hash)
+          params[:scope_opts].each do |k,v|
+            params["scope_#{k}".to_sym] = v
+          end
+          params.delete(:scope_opts)
+        end
+        if params[:metadata].is_a?(Hash)
+          params[:metadata].each do |k,v|
+            params["metadata_#{k}".to_sym] = v
+          end
+          params.delete(:metadata)
+        end
+
         return get("/v1/lookup/#{key}", params)
       end
     end
